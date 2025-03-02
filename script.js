b990dd8230348d04948c0813dfcd9c658fe4465d
Alpeshmore <morealpesh.45@gmail.com>
2025-03-02 19:20:15 +0530

    Initial commit - Multi-page website

---
+++ script.js
@@ -0,0 +1,9 @@
+// Load navbar into all pages
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading navbar:", error));
+});
